1f2a34cabec7f806b95ffe43017372dd52cb6e7e
MeganBishop14 <meganbishop01@icloud.com>
2020-07-31 15:45:05 +0000

    fifth try

--- model.py
+++ model.py
@@ -10,24 +10,29 @@ def get_password(length, number, capitalization, special):
     letters = string.ascii_lowercase
     numbers = string.digits
     punctuation = string.punctuation
+    if int(length)<3:
+        length = 3
     for x in range (0, int(length)):
     #add one random character each loop
         password += letters[random.randrange(0,len(letters))]
     passlist= list(password)
     #STRING METHODS
+    position_number=random.randrange(0,len(passlist))
+    position_capitalization=random.randrange(0,len(passlist))
+    position_special=random.randrange(0,len(passlist))
     if number:
-        position=random.randrange(0,len(passlist))
-        passlist[position]= numbers[random.randrange(0,len(numbers))]
+        passlist[position_number]= numbers[random.randrange(0,len(numbers))]
 #get random index (hint method) of the password and replace it (hint method) with a random number from 0-9
 #random.randint(0,9)
     if capitalization:
-        position=random.randrange(0,len(passlist))
-        passlist[position]= (passlist[position]).upper()
+        while position_capitalization == position_number:
+            position_capitalization=random.randrange(0,len(passlist))
+        passlist[position_capitalization]= (passlist[position_capitalization]).upper()
 #get random index and make that character capital (method)
     if special:
-        position=random.randrange(0,len(passlist))
-        passlist[position]= punctuation[random.randrange(0,len(punctuation))]
-       
+        while position_capitalization == position_special or position_special == position_number:
+            position_special=random.randrange(0,len(passlist))
+        passlist[position_special]= punctuation[random.randrange(0,len(punctuation))]
         #(similar to the number) you will replace a random index with a special
     password = " ".join(passlist)
     return password
